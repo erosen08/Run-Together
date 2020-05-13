8a253b522c6b7f62240dddf43913ae1b0ebb65b3
erosen08 <erosenberg39@gmail.com>
2020-05-13 14:26:18 -0400

    more css

--- app/javascript/react/components/GroupTile.js
+++ app/javascript/react/components/GroupTile.js
@@ -6,7 +6,7 @@ const GroupTile = (props) => {
   return (
     <div className="group-name callout">
       <h3>
-        <Link to={`/groups/${props.group.id}`}>{props.group.name}</Link>
+        <Link to={`/groups/${props.group.id}`} className="group-title">{props.group.name}</Link>
       </h3>
     </div>
   )
