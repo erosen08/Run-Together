4fa20d47033580b24d3eb982b5d98d54303d522b
erosen08 <erosenberg39@gmail.com>
2020-05-17 17:46:32 -0400

    small fix

--- app/javascript/react/components/GroupShowTile.js
+++ app/javascript/react/components/GroupShowTile.js
@@ -11,11 +11,11 @@ const GroupShowTile = props => {
         <h5 className="description-run">Description:</h5>
         <p>{props.group.description}</p>
         <div className="grid-x">
-          <div className="cell small-6 difficulty">
+          <div className="cell small-6 difficulty-run">
             <h6>Difficulty:</h6>
             <p>{props.group.difficulty}</p>
           </div>
-          <div className="cell small-6 zip">
+          <div className="cell small-6 zip-run">
             <h6>Zip Code:</h6>
             <p>{props.group.zip}</p>
           </div>
